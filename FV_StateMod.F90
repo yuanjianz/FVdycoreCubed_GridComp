c4f37be4f698192d30a9837b79032cda99202f7e
William Putman <William.M.Putman@nasa.gov>
2022-02-03 17:09:25 -0500

    fixed units of mfz

--- FV_StateMod.F90
+++ FV_StateMod.F90
@@ -3119,8 +3119,6 @@ subroutine fv_getVerticalMassFlux(mfx, mfy, mfz, dt)
   real(REAL8) :: conv(FV_Atm(1)%bd%isc:FV_Atm(1)%bd%iec,FV_Atm(1)%bd%jsc:FV_Atm(1)%bd%jec,1:FV_Atm(1)%npz)
   real(REAL8) :: pit(FV_Atm(1)%bd%isc:FV_Atm(1)%bd%iec,FV_Atm(1)%bd%jsc:FV_Atm(1)%bd%jec)
 
-  real(REAL8) :: fac
-
   real(REAL8) :: wbuffer(FV_Atm(1)%bd%jsc:FV_Atm(1)%bd%jec,FV_Atm(1)%npz)
   real(REAL8) :: sbuffer(FV_Atm(1)%bd%isc:FV_Atm(1)%bd%iec,FV_Atm(1)%npz)
   real(REAL8) :: ebuffer(FV_Atm(1)%bd%jsc:FV_Atm(1)%bd%jec,FV_Atm(1)%npz)
@@ -3167,8 +3165,6 @@ subroutine fv_getVerticalMassFlux(mfx, mfy, mfz, dt)
      enddo
   end if
 
-  fac = 1.0/(dt*MAPL_GRAV)
-!
 ! Compute the vertical mass flux
 !
 !   Compute Convergence of the horizontal Mass flux
@@ -3176,7 +3172,7 @@ subroutine fv_getVerticalMassFlux(mfx, mfy, mfz, dt)
        do j=jsc,jec
           do i=isc,iec
              conv(i,j,k) = ( xfx(i,j,k) - xfx(i+1,j,k) +  &
-                             yfx(i,j,k) - yfx(i,j+1,k) ) * fac
+                             yfx(i,j,k) - yfx(i,j+1,k) )
           enddo
        enddo
     enddo
